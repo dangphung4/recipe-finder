e4410f3bc3996cb3dc07d7b379ddf7d6eaa2eb20
Your Name <you@example.com>
2023-08-23 17:08:25 -0400

    added home button route

--- app.py
+++ app.py
@@ -5,4 +5,12 @@ from urllib.parse import unquote
 #flask app
 app = Flask(__name__)
 
+#env SPOONTACULAR API KEY
+API_KEY = "832820078b2e475f913f9fbb5f2fe382"
+
+#home button route
+@app.route("/home", methods=["GET"])
+def home():
+    #render main page, empty list and serach query
+    return render_template("index.html", recipe=[], search_query="")
 
