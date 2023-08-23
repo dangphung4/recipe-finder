424433de394a3c75aad434c1c7a664f59d4ac67e
Your Name <you@example.com>
2023-08-23 17:49:39 -0400

    app draft changes

--- app.py
+++ app.py
@@ -2,73 +2,72 @@ from flask import Flask, render_template, request
 import requests
 from urllib.parse import unquote
 
-#flask app
+# Create the flask app
 app = Flask(__name__)
 
-#env SPOONTACULAR API KEY
-API_KEY = "832820078b2e475f913f9fbb5f2fe382"
+# Replace with your Spoonacular API key
+API_KEY = '832820078b2e475f913f9fbb5f2fe382'
 
-#home button route
-@app.route("/home", methods=["GET"])
+# Define the route for the "Home" button
+@app.route('/home', methods=['GET'])
 def home():
-    #render main page, empty list and serach query
-    return render_template("index.html", recipe=[], search_query="")
+    # Render the main page with empty recipe list and search query
+    return render_template('index.html', recipes=[], search_query='')
 
-#main route for app
-@app.route("/", methods=["GET", "POST"])
-def index(): 
-    if request.method == "POST":
-        #if form is submitted
-        query = request.form.get("search_query", "")
-        # perform seasrch for recipes with given query
+# Define the main route for the app
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        query = request.form.get('search_query', '')
         recipes = search_recipes(query)
-        #render main page with search results and search query
-        return render_template("index.html", recipe=recipes, search_query=query)
-        
-    #if no form is submitted
-    search_query = request.args.get("search_query", "")
+        return render_template('index.html', recipes=recipes, search_query=query)
+    
+    # If it's a GET request or no form submitted
+    search_query = request.args.get('search_query', '')
     decoded_search_query = unquote(search_query)
-    # perform a search for recipes with the decoded search query
+    # Perform a search for recipes with the decoded search query
     recipes = search_recipes(decoded_search_query)
-    #render main page
-    return render_template("index.html", recipes=recipes, search_query=decoded_search_query)
+    # Render the main page
+    return render_template('index.html', recipes=recipes, search_query=decoded_search_query)
 
-# search function upon query
+# Function to search for recipes based on the provided query
 def search_recipes(query):
-    url = f"https://api.spoonacular.com/recipes/complexSearch"
+    url = f'https://api.spoonacular.com/recipes/complexSearch'
     params = {
-        "apiKey": API_KEY,
-        "query" : query,
-        "number": 10,
-        "instructionsRequired": True,
-        "addRecipeInformation": True,
-        "fillIngredients" : True,
+        'apiKey': API_KEY,
+        'query': query,
+        'number': 10,
+        'instructionsRequired': True,
+        'addRecipeInformation': True,
+        'fillIngredients': True,
     }
 
-    # Send get request to API
+    # Send a GET request to the Spoonacular API with the query parameters
     response = requests.get(url, params=params)
-    if response.status_code== 200:
+    if response.status_code == 200:
+        # Parse the API response as JSON data
         data = response.json()
-
-        return data["results"]
+        return data['results']
     return []
 
-#route to view specific recipe
-@app.route("/recipe/<int:recipe_id>")
+# Route to view a specific recipe with a given recipe ID
+@app.route('/recipe/<int:recipe_id>')
 def view_recipe(recipe_id):
-    search_query = request.args.get("search_query", "")
-    url = f"https://api.spoonacular.com/recipes/{recipe_id}/information"
+    # Get the search query from the URL query parameters
+    search_query = request.args.get('search_query', '')
+    # Build the URL to get information about the specific recipe ID from Spoonacular API
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
     params = {
-        "apiKey": API_KEY,
+        'apiKey': API_KEY,
     }
 
+    # Send a GET request to the Spoonacular API to get the recipe information
     response = requests.get(url, params=params)
-    if response.status_code== 200:
-        data = response.json()
-
-        return render_template("view_recipe.html", recipe=recipe, search_query=search_query)
+    if response.status_code == 200:
+        recipe = response.json()
+        return render_template('view_recipe.html', recipe=recipe, search_query=search_query)
     return "Recipe not found", 404
 
-#Run app
-if __name__== "__main__":
+# Run the app in debug mode 
+if __name__ == '__main__':
     app.run(debug=True)
